6c236f7844b21efc4e1b26ef296181b4b2034a94
Bharat Dua <bharatdua13@gmail.com>
2025-09-27 14:41:07 +0530

    feat: added the top announcement bar

---
+++ src/components/TopNotificationBar/TopNotificationBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './TopNotificationBar.css';
+
+
+function TopNotificationBar() {
+  return (
+    <>
+      <div className="text-center py-1" style={{backgroundColor: "#fb7f88"}}>
+        <a href="#" className="hover-link-underline" style={{fontWeight:500}}>Diwali Sale is LIVE!</a>
+      </div>
+    </>
+  );
+}
+
+export default TopNotificationBar;
